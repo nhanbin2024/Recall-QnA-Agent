77fce34e4b644a9c75e97e65623eca5682f2dead
JINCHAIN <phamducnhan2006@gmail.com>
2025-10-16 13:55:48 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -24,7 +24,8 @@ if __name__ == "__main__":
   print(get_response(user_input))
   print(get_response(user_input))
   print(get_response(user_input))
-  print(get_response(user_input))
+  print(get_response(user_input))# Lấy API key từ biến môi trường
+openai.api_key = os.getenv("OPENAI_API_KEY")
   print(get_response(user_input))
   print(get_response(user_input))
   print(get_response(user_input))
